32710238018f8393a587add279a343f694490030
Justin <jlang61@ucsb.edu>
2022-10-25 17:04:08 -0700

    finish pow

--- pow.asm
+++ pow.asm
@@ -15,14 +15,73 @@
 # Assembly (NON-RECURSIVE) code version of pow(x,n):
 #
 .data
-
+	number: .asciiz "Enter the number x:\n"
+	exponent: .asciiz "Enter the exponent n:\n"
+	output: .asciiz "Power(x,n) is:\n"
+	newline: .asciiz "\n"
 	# TODO: Write your initializations here
 
 #Text Area (i.e. instructions)
 .text
 main:
+	# t0 contains number while t1 contains exponent
+	li $v0 4 
+	la $a0 number
+	syscall 
+
+	li $v0 5 
+	syscall 
+	move $t0 $v0 
+
+	li $v0 4
+	la $a0 exponent
+	syscall 
+
+	li $v0 5 
+	syscall 
+	move $t1 $v0 
 	
-	# TODO: Write your code here
+
+	
+	
+	# t2 contains copy of t0
+	move $t2 $t0 
+
+
+
+	bne $t1 0 loop
+
+	li $t2 1
+	j print
+	 
+
+
+
+loop:
+
+	beq $t1, 1, print	
+	mult $t2, $t0 
+	mflo $t2 
+	addi $t1, $t1, -1
+	j loop 
+
+print:
+	
+	li $v0 4 
+	la $a0 output
+	syscall 
+
+	li $v0 1 
+	move $a0 $t2 
+	syscall 
+
+	li $v0 4 
+	la $a0 newline
+	syscall
+	 
+	j exit
 
 exit:
+	li $v0 10 
+	syscall 
 	# TODO: Write code to properly exit a SPIM simulation
